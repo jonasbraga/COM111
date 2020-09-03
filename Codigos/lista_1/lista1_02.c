187be9fba053e418ce8003b4de2c8d26bd19a193
Jonas Braga <jonasbraga2001@gmail.com>
2020-09-03 04:56:35 +0000

    feat: :spark: lista1_02.c created

--- Codigos/lista_1/lista1_02.c
+++ Codigos/lista_1/lista1_02.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+const float APPROVED_AVERAGE = 6.0;
+
 struct Aluno {
   int matricula;
   char nome [50];
@@ -10,59 +12,87 @@ struct Aluno {
   float nota;
 };
 
-int *createStudentsArray(int n);
-void fillStudents();
-void showStudents();
-void fillStudentData();
-void showStudentData();
+typedef struct Aluno ALUNO;
+
+ALUNO* createStudentsArray(int n);
+void showApprovedStudents(int n, ALUNO *alunos);
+void fillStudents(int n, ALUNO *alunos);
+void showApprovedStudents(int n, ALUNO *alunos);
+void fillStudentData(ALUNO *aluno);
+void showStudentData(ALUNO aluno);
 
 int main(){
-
-  int n, *aluno;
+  int n;
+  ALUNO *alunos;
 
   printf("Insira o numero de alunos: ");
   scanf("%d", &n);
-  
-  
+ 
+  alunos = createStudentsArray(n);
+
+  fillStudents(n, alunos);
+
+  showApprovedStudents(n, alunos);
+
   return 0;
 }
 
-// ler numero n de alunos a serem cadastrados
+// Aloca um espaço dinamico na memoria para os alunos
+ALUNO* createStudentsArray(int n) {
 
-// função com um V[n] que retorna um *ALUNO
-
-// preencher vetor V[n]
-
-// mostrar alunos aprovados (nota>=6)
-
-int* createStudentsArray(int n) {
-
-// cria os vetores suas quantidades seus atributos do registro
-  struct Aluno *alunos;
-  alunos = malloc(n * sizeof(struct Aluno));
-return alunos;
+  ALUNO *alunos = malloc(n * sizeof(ALUNO));
+  return alunos;
 }
 
-void fillStudents() {
+void fillStudents(int n, ALUNO *alunos) {
 
+  for(int i = 0;i < n; i++){
+    printf("\n%dº aluno: \n", i+1);
+    fillStudentData(&alunos[i]);   
+    printf("\n");
+  }
 }
 
-void showStudents() {
+// Mostra os alunos aprovados (nota>=6)
+void showApprovedStudents(int n, ALUNO *alunos) {
 
+  printf("\n==== ALUNOS APROVADOS ====\n");
+
+  for(int i = 0;i < n; i++){
+    if(alunos[i].nota < APPROVED_AVERAGE) continue;
+    printf("\n %dº aluno: \n", i+1);
+    showStudentData(alunos[i]);
+    printf("\n");
+  }
 }
 
-void fillStudentData() {
+// Pede os dados do aluno para o usuário e salva
+void fillStudentData(ALUNO *aluno) {
+  printf("Matricula: ");
+  scanf("%d", &aluno->matricula);
 
+  printf("Nome: ");
+  scanf(" %[^\n]s", aluno->nome);
+
+  printf("Dia de nascimento: ");
+  scanf(" %d", &aluno->dia_nasc);
+
+  printf("Mês de nascimento: ");
+  scanf("%d", &aluno->mes_nasc);
+
+  printf("Ano de nascimento: ");
+  scanf("%d", &aluno->ano_nasc);
+
+  printf("Nota: ");
+  scanf("%f", &aluno->nota);
 }
 
-void showStudentData() {
-  
+// Mostra um aluno
+void showStudentData(ALUNO aluno) {
+  printf("\nMatricula: %d", aluno.matricula);
+  printf("\nNome: %s", aluno.nome);
+  printf("\nDia de nascimento: %d", aluno.dia_nasc);
+  printf("\nMês de nascimento: %d", aluno.mes_nasc);
+  printf("\nAno de nascimento: %d", aluno.ano_nasc);
+  printf("\nNota: %0.2f", aluno.nota);
 }
-
-/*
-  (a) Alocar memória dinamicamente para um vetor do tipo ALUNO.
-  (b) Preencher o vetor de alunos.
-  (c) Imprimir o vetor de alunos.
-  (d) Preencher o registro de um aluno.
-  (e) Imprimir o registro de um aluno. 
-*/
