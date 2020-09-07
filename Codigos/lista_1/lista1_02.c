b049db5438f6d2a37d4f094e6790d22fb869ae23
Jonas Braga <jonasbraga2001@gmail.com>
2020-09-07 02:32:20 +0000

    Renamed exercise description, added free  to ex2

--- Codigos/lista_1/lista1_02.c
+++ Codigos/lista_1/lista1_02.c
@@ -25,7 +25,7 @@ int main(){
   int n;
   ALUNO *alunos;
 
-  printf("Insira o numero de alunos: ");
+  printf("\n Insira a quantidade de alunos: ");
   scanf("%d", &n);
  
   alunos = createStudentsArray(n);
@@ -33,6 +33,8 @@ int main(){
   fillStudents(n, alunos);
 
   showApprovedStudents(n, alunos);
+
+  free(alunos);
 
   return 0;
 }
