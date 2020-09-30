daf218d8c3534932a734ba329f4604b27973739c
Jonas Braga <jonasbraga2001@gmail.com>
2020-09-30 02:17:56 +0000

    feat: base files to lista_3 and trabalho_1 exercises

---
+++ Codigos/trabalho_1/main.c
@@ -0,0 +1,15 @@
+// Comando para compilar e executar
+// gcc ./Codigos/trabalho_1/main.c ./Codigos/trabalho_1/example.h ./Codigos/trabalho_1/example.c -o exe -lm && ./exe 
+
+// bibliotecas do sistema
+#include <stdio.h>
+#include <stdlib.h>
+
+// bibliotecas do projeto
+#include "example.h"
+
+// funcao principal
+int main(void) {
+
+  
+}
